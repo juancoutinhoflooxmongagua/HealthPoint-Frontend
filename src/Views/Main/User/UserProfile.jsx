13cfef3e77faa540f64b7e352ea84396cbd8bdc8
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-02 10:45:35 -0300

    feat: add Profile with connection for Backend | data corrects

--- src/Views/Main/User/UserProfile.jsx
+++ src/Views/Main/User/UserProfile.jsx
@@ -1,9 +1,55 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { AuthContext } from "../../../Context/authContext";
 export default function UserProfile() {
-   return (
+
+    
+  const { user } = useContext(AuthContext); 
+  return (
+    
     <main>
-        <h1>Perfil</h1>
+      <h1>Perfil</h1>
+
+      <div className="col-lg-8">
+        <div className="card mb-4">
+          <div className="card-body">
+            <div className="row">
+              <div className="col-sm-3">
+                <p className="mb-0">Full Name</p>
+              </div>
+              <div className="col-sm-9">
+                <p className="text-muted mb-0">{user.user_name}</p>
+              </div>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="col-sm-3">
+                <p className="mb-0">Email</p>
+              </div>
+              <div className="col-sm-9">
+                <p className="text-muted mb-0">{user.user_email}</p>
+              </div>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="col-sm-3">
+                <p className="mb-0">Phone</p>
+              </div>
+              <div className="col-sm-9">
+                <p className="text-muted mb-0">{user.user_phone}</p>
+              </div>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="col-sm-3">
+                <p className="mb-0">Role</p>
+              </div>
+              <div className="col-sm-9">
+                <p className="text-muted mb-0">{user.user_role}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
-   ) 
+  );
 }
