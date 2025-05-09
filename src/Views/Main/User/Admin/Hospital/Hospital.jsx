f5daf85359c3f585fcd8c2ce5afb8bf3aa96b58b
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-05-09 13:25:09 -0300

    feat: list Hospitals

--- src/Views/Main/User/Admin/Hospital/Hospital.jsx
+++ src/Views/Main/User/Admin/Hospital/Hospital.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Hospital() {
+    const [hospitals, setHospitals] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://healthpoint-backend-production.up.railway.app/hospital", {
+
+        })
+        .then((res) => {
+            setHospitals(res.data); 
+        })
+        .catch((err) => {
+            console.error(err); 
+        });
+    }, ); 
+
     return (
         <main className="container mt-5">
-            <h1 className="text-center text-primary mb-4" style={{ fontWeight: '600' }}>Hospitais</h1>
-            <div className="card shadow-sm rounded-3 border-0">
-                <div className="card-body">
-                    <p className="text-muted mb-0">Nenhum hospital encontrado.</p>
+            <h1 className="text-center text-primary mb-4" style={{ fontWeight: '600' }}>Hospitais Cadastrados</h1>
+            {hospitals.length > 0 ? ( 
+                <div className="card shadow-sm rounded-3 border-0">
+                    <div className="card-body">
+                        <ul>
+                            {hospitals.map((hospital) => (
+                                <div className="border border-primary p-2" key={hospital.hospital_id}>
+                                     <h2>{hospital.hospital_name}</h2>
+                                    <h6><strong>Endereço:</strong> {hospital.hospital_address}</h6>
+                                    <h6><strong>Telefone:</strong> {hospital.hospital_phone}</h6>
+                                    <h6> <strong>Criado no Sistema em:</strong> {hospital.created_at}</h6>
+                                </div> 
+                            ))}
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <div className="card shadow-sm rounded-3 border-0">
+                    <div className="card-body">
+                        <p className="text-muted mb-0">Nenhum hospital encontrado.</p>
+                    </div>
+                </div>
+            )}
         </main>
     );
 }
